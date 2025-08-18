2a6ae53591c94d7d85b6b5a74d7f10411e519677
jasbirsingh <jasbir@thecodelab.tech>
2025-08-18 14:02:35 +0530

    latest

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2515036795"
+    serviceWorkerVersion: "469030966"
   }
 });
